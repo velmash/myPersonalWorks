0db802da61cf7853b05218b525095252cc78407c
velmash <hyengchan@gmail.com>
2023-01-10 22:21:12 +0900

    :fire: 프로젝트 세분화 테스트 1

---
+++ GitUploadTest/GitUploadTest/GitUploadTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitUploadTestApp.swift
+//  GitUploadTest
+//
+//  Created by 윤형찬 on 2023/01/10.
+//
+
+import SwiftUI
+
+@main
+struct GitUploadTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
