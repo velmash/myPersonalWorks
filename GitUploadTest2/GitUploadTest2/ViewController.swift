bca20d2ce3f176f0c9fb41929b81a4a558bf3c0e
velmash <hyengchan@gmail.com>
2023-01-10 22:21:55 +0900

    :fire: 프로젝트 세분화 테스트 2

---
+++ GitUploadTest2/GitUploadTest2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitUploadTest2
+//
+//  Created by 윤형찬 on 2023/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
